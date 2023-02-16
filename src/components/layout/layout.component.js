bc8d6dc5a0d13892c8fb8a1ee742c2be17329cda
Lachlan Whelan <lachlan.whelan1@gmail.com>
2023-02-16 21:28:30 +1100

    Create basic footer component

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../footer/footer.component";
 import Navbar from "../navbar/navbar.component";
 
 const Layout = () => {
     return(
         <>
             <Navbar/>
+            <main>
                 <Outlet/>
-            <footer>FOOTER</footer>
+            </main>
+            <Footer/>
         </>
     )
 }
