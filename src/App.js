bb4e3b5fc4c51bf5f29e50a6fd7b167feb509812
Lachlan Whelan <lachlan.whelan1@gmail.com>
2023-02-16 21:26:07 +1100

    Import new shared style files at top level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+
+import 'normalize.css';
+import './styles/resets.scss';
+import './styles/global.scss';
+
 import Layout from "./components/layout/layout.component";
 import Home from "./pages/home/home.component";
 import Movies from "./pages/movies/movies.component";
 import NotFound from "./pages/notfound/notfound.component";
 import People from "./pages/people/people.componet";
 import Shows from "./pages/shows/shows.component";
+
 
 
 const App = () => {
